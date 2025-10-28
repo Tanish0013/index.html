8ae5e8fa3adf0b08a270161b5159eaf6051ef18f
Tanish0013 <tanishchopra0013@gmail.com>
2025-10-28 15:39:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+async function sendMessage() {
+  const userInput = document.getElementById("userInput").value;
+  addMessage(userInput, "user");
+
+  const response = await fetch("/api/chat", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ message: userInput }),
+  });
+
+  const data = await response.json();
+  addMessage(data.reply, "bot");
+}
